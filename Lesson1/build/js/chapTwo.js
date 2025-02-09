47ce4f2207bca0b498ae997ea7db9de9bba4e1fc
Anezijane <ahnnieanezi@gmail.com>
2025-02-09 22:37:37 +0100

    Types

--- Lesson1/build/js/chapTwo.js
+++ Lesson1/build/js/chapTwo.js
@@ -3,8 +3,16 @@ let isRightColor;
 let day;
 let acc;
 let stat;
+let album; // union type
+let postId;
+let isActive;
+let regex = /\w+/g;
 isRightColor = true;
 day = "Monday";
 acc = 0.5;
 stat = "hello";
+album = 123; // 'joey'
 console.log(`${stat} ${day} ${acc} ${isRightColor} of everything.`);
+let sum = (a, b) => {
+    return a + b;
+};
--- Lesson1/src/chapTwo.ts
+++ Lesson1/src/chapTwo.ts
@@ -2,10 +2,21 @@ let isRightColor: boolean;
 let day: string;
 let acc: number;
 let stat: any;
+let album: string | number; // union type
+let postId: string | number;
+let isActive: number | boolean;
+
+let regex: RegExp = /\w+/g;
 
 isRightColor = true;
 day = "Monday";
 acc = 0.5;
 stat = "hello";
+album = 123; // 'joey'
+
 
 console.log(`${stat} ${day} ${acc} ${isRightColor} of everything.`);
+
+let sum = (a:number, b:number) => {
+    return a + b;
+}
