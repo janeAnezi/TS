2208f21a10346c32574b0e0cd1031e29020b3afb
Anezijane <ahnnieanezi@gmail.com>
2025-02-10 19:28:53 +0100

    arrays

---
+++ Lesson1/build/js/chapThree.js
@@ -0,0 +1,5 @@
+"use strict";
+let stringArr = ['Joey', 'Amelia', 'Brook'];
+let dataArr = ['Anon', 236, 'Solo'];
+let allDataArr = [56, 'bobby', true];
+stringArr.push('bae');
---
+++ Lesson1/src/chapThree.ts
@@ -0,0 +1,6 @@
+ 
+ let stringArr = ['Joey', 'Amelia', 'Brook'];
+ let dataArr = ['Anon', 236, 'Solo'];
+ let allDataArr = [56, 'bobby', true];
+
+stringArr.push('bae');
