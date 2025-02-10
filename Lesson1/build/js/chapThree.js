d876cdadf4b7895a9689f5c49d58198aef070c4d
Anezijane <ahnnieanezi@gmail.com>
2025-02-10 20:36:06 +0100

    tuple

--- Lesson1/build/js/chapThree.js
+++ Lesson1/build/js/chapThree.js
@@ -3,3 +3,17 @@ let stringArr = ['Joey', 'Amelia', 'Brook'];
 let dataArr = ['Anon', 236, 'Solo'];
 let allDataArr = [56, 'bobby', true];
 stringArr.push('bae');
+dataArr.push(123);
+allDataArr = stringArr;
+//dataArr[0] = false;
+stringArr[1] = 'samson';
+dataArr.unshift(77);
+let empArr = [];
+let bands = [];
+bands.push('blessing');
+// Tuple - must contain the types defined
+let myTup = [45, 'ann', false];
+let other = [56, 'clarc', true];
+//myTup = other;  
+other = myTup;
+myTup[1] = 'glory';
--- Lesson1/src/chapThree.ts
+++ Lesson1/src/chapThree.ts
@@ -4,3 +4,22 @@
  let allDataArr = [56, 'bobby', true];
 
 stringArr.push('bae');
+dataArr.push(123);
+allDataArr = stringArr;
+//dataArr[0] = false;
+stringArr[1] = 'samson';
+dataArr.unshift(77);
+
+let empArr = [];
+let bands: string[] = [];
+bands.push('blessing');
+
+// Tuple - must contain the types defined
+let myTup: [number, string, boolean] = [45, 'ann', false]
+let other = [56, 'clarc', true];
+//myTup = other;  
+other = myTup;
+
+myTup[1] = 'glory';
+
+
